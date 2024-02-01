ce882eadd2bfc111b2d390a80ca621dd6f090bce
Ibnu-Faqri <116130577+Ibnu-Faqri@users.noreply.github.com>
2024-02-02 01:29:41 +0700

    Delete server directory

--- server/routes/Users/index.js
+++
@@ -1,8 +0,0 @@
-const UserController = require("../../controller/UserController");
-const router = require("express").Router();
-
-router.post("/register", UserController.register);
-router.post("/login", UserController.login);
-
-
-module.exports = router;
